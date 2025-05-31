12abf27a93a721a924f6c961d279119b983c5dd8
Muktaka Joshipura <muktaka2000@gmail.com>
2025-05-31 00:24:38 -0500

    feat(user): Add username

--- lib/user/model.py
+++ lib/user/model.py
@@ -1,34 +1,44 @@
+from typing import Tuple
+
+import sqlite3
+
 from ..common import exceptions
 
-class UserSession(object):
-    def __init__(self, user_id):
+class User(object):
+    def __init__(self, user_id: int, username: str):
         self.user_id = user_id
+        self.username = username
 
 class UserModel(object):
-    def __init__(self, connection):
+    def __init__(self, connection: sqlite3.Connection):
         self.connection = connection
     
-    def create_session(self, user_name):
+    def create_session(self, user_name: str):
         cur = self.connection.cursor()
-        users = cur.execute('select (users.id) from users where users.user_name = ? limit 1', (user_name,)).fetchall()
+        users = cur.execute('''
+            select users.id, users.user_name from users
+            where users.user_name = ? limit 1''',
+            (user_name,)).fetchall()
         if len(users) == 0:
             raise exceptions.NotFound
         else:
-            return UserSession(users[0][0])
+            user_result: Tuple[int, str] = users[0]
+            user_id, username = user_result
+            return User(user_id, username)
     
-    def create_user(self, user_name):
+    def create_user(self, user_name: str):
         cur = self.connection.cursor()
         try:
-            result = cur.execute('insert into users (user_name) values (?) returning users.id', (user_name,)).fetchone()
+            result = cur.execute('insert into users (user_name) values (?) returning (users.id, users.user_name)', (user_name,)).fetchone()
             self.connection.commit()
-            return UserSession(result[0])
+            return User(result[0], result[1])
         except:
             raise exceptions.NotModified
     
-    def delete_user(self, user_name):
+    def delete_user(self, user_name: str):
         cur = self.connection.cursor()
         try:
-            result = cur.execute('delete from users where users.user_name = ?', (user_name,))
+            cur.execute('delete from users where users.user_name = ?', (user_name,))
             self.connection.commit()
         except:
             raise exceptions.NotModified
