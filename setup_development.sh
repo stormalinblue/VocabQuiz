65c0d331d0e1234377b7b0c684040d49845076b0
Muktaka Joshipura <muktaka2000@gmail.com>
2025-06-17 03:48:29 -0500

    fix: Notebook stripping scripts

--- setup_development.sh
+++ setup_development.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-git config --global filter.strip-notebook-output.clean "echo $pwd; ./venv/bin/python3 -m jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR"
+git config --local filter.strip-notebook-output.clean "\$PWD/venv/bin/python3 -m jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR"
